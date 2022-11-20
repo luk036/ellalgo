bb80eb2f7a1d3d629e213afcf072bcd022b6614a
Wai-Shing Luk <luk036@gmail.com>
2022-11-20 09:00:37 +0000

    add equation description

--- src/ellalgo/ell_calc.py
+++ src/ellalgo/ell_calc.py
@@ -20,6 +20,11 @@ class EllCalc:
     cst3: float
 
     def __init__(self, n: float) -> None:
+        """_summary_
+
+        Args:
+            n (float): _description_
+        """
         self.n_f = n
         self.half_n = n / 2.0
         self.cst0 = 1.0 / (n + 1.0)
@@ -42,8 +47,37 @@ class EllCalc:
         return E
 
     # def update_cut(self, beta: float) -> CutStatus { self.calc_dc(beta)
-    def calc_ll(self, beta) -> CutStatus:
+    def calc_ll(self, beta1) -> CutStatus:
         """parallel or deep cut
+
+                       __________________________
+                      ╱                         2
+                     ╱                  ⎛     2⎞ 
+                    ╱                   ⎜n ⋅ β ⎟ 
+                   ╱   ⎛ 2    2⎞    2   ⎜     1⎟ 
+            ξ =   ╱    ⎜τ  - β ⎟ ⋅ τ  + ⎜──────⎟ 
+                ╲╱     ⎝      1⎠        ⎝   2  ⎠ 
+
+                            ⎛ 2    ⎞             
+                  n     2 ⋅ ⎝τ  - ξ⎠             
+            σ = ───── + ────────────             
+                n + 1              2             
+                        (n + 1) ⋅ β              
+                                   1             
+
+                σ ⋅ β                            
+                     1                           
+            ϱ = ──────                           
+                   2                             
+
+                     ⎛      2    ⎞               
+                     ⎜     β     ⎟               
+                 2   ⎜ 2    1   ξ⎟               
+                n  ⋅ ⎜τ  - ── + ─⎟               
+                     ⎝      2   n⎠               
+            δ = ──────────────────               
+                   ⎛ 2    ⎞    2                 
+                   ⎝n  - 1⎠ ⋅ τ                  
 
         Arguments:
             beta ([type]): [description]
@@ -51,13 +85,58 @@ class EllCalc:
         Returns:
             int: [description]
         """
-        if np.isscalar(beta):
-            return self.calc_dc(beta, sqrt(self.tsq))
-        elif len(beta) < 2:  # unlikely
-            return self.calc_dc(beta[0], sqrt(self.tsq))
-        return self.calc_ll_core(beta[0], beta[1])
+        if np.isscalar(beta1):
+            return self.calc_dc(beta1, sqrt(self.tsq))
+        elif len(beta1) < 2:  # unlikely
+            return self.calc_dc(beta1[0], sqrt(self.tsq))
+        return self.calc_ll_core(beta1[0], beta1[1])
 
     def calc_ll_core(self, b0: float, b1: float) -> CutStatus:
+        """Parallel Cut
+
+                  2    2                         
+            ζ  = τ  - β                          
+             0         0
+
+                  2    2                         
+            ζ  = τ  - β                          
+             1         0                         
+                       __________________________
+                      ╱                         2
+                     ╱           ⎛    ⎛ 2    2⎞⎞ 
+                    ╱            ⎜n ⋅ ⎜β  - β ⎟⎟ 
+                   ╱             ⎜    ⎝ 1    0⎠⎟ 
+            ξ =   ╱    ζ  ⋅ ζ  + ⎜─────────────⎟ 
+                ╲╱      0    1   ⎝      2      ⎠ 
+
+                            ⎛ 2              ⎞   
+                        2 ⋅ ⎜τ  - β  ⋅ β  - ξ⎟   
+                  n         ⎝      0    1    ⎠   
+            σ = ───── + ──────────────────────   
+                n + 1                       2    
+                         (n + 1) ⋅ ⎛β  + β ⎞     
+                                   ⎝ 0    1⎠     
+
+                σ ⋅ ⎛β  + β ⎞                    
+                    ⎝ 0    1⎠                    
+            ϱ = ─────────────                    
+                      2            
+
+                     ⎛ζ  + ζ     ⎞               
+                 2   ⎜ 0    1   ξ⎟               
+                n  ⋅ ⎜─────── + ─⎟               
+                     ⎝   2      n⎠               
+            δ = ──────────────────               
+                   ⎛ 2    ⎞    2                 
+                   ⎝n  - 1⎠ ⋅ τ                  
+
+        Args:
+            b0 (float): _description_
+            b1 (float): _description_
+
+        Returns:
+            CutStatus: _description_
+        """
         if b1 < b0:
             return CutStatus.NoSoln  # no sol'n
         if (tau := sqrt(self.tsq)) < b1 or not self.use_parallel_cut:
@@ -80,6 +159,14 @@ class EllCalc:
         return CutStatus.Success
 
     def calc_ll_cc(self, b1: float) -> CutStatus:
+        """_summary_
+
+        Args:
+            b1 (float): _description_
+
+        Returns:
+            CutStatus: _description_
+        """
         b1sq = b1**2
         xi = sqrt((self.tsq - b1sq) * self.tsq + (self.half_n * b1sq) ** 2)
         self.sigma = self.cst3 + self.cst2 * (self.tsq - xi) / b1sq
@@ -89,6 +176,31 @@ class EllCalc:
         return CutStatus.Success
 
     def calc_dc(self, beta: float, tau: float) -> CutStatus:
+        """Deep Cut
+
+            γ = τ + n ⋅ β        
+
+                  γ
+            ϱ = ─────
+                n + 1            
+
+                2 ⋅ ϱ      
+            σ = ─────
+                τ + β
+
+                 2   ⎛ 2    2⎞   
+                n  ⋅ ⎝τ  - β ⎠   
+            δ = ──────────────   
+                 ⎛ 2    ⎞    2   
+                 ⎝n  - 1⎠ ⋅ τ    
+
+        Args:
+            beta (float): _description_
+            tau (float): _description_
+
+        Returns:
+            CutStatus: _description_
+        """
         if tau < beta:
             return CutStatus.NoSoln  # no sol'n
         if beta == 0.0:
@@ -103,12 +215,37 @@ class EllCalc:
         return CutStatus.Success
 
     def calc_cc(self, tau: float) -> CutStatus:
+        """Central Cut
+
+                  2   
+            σ = ───── 
+                n + 1 
+                  τ   
+            ϱ = ───── 
+                n + 1 
+                   2  
+                  n   
+            δ = ──────
+                 2    
+                n  - 1
+
+        Args:
+            tau (float): _description_
+
+        Returns:
+            CutStatus: _description_
+        """
         self.sigma = self.cst2
         self.rho = self.cst0 * tau
         self.delta = self.cst1
         return CutStatus.Success
 
     def get_results(self) -> List[float]:
+        """_summary_
+
+        Returns:
+            List[float]: _description_
+        """
         return [self.rho, self.sigma, self.delta, self.tsq]
 
 
